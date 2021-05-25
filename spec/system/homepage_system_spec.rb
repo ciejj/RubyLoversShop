af816efe9aee199398bb1f51eded4de5e65ca758
ciejj <wozniczka.mateusz@gmail.com>
2021-05-25 13:42:15 +0000

    test: Home Page displays proper number of products

--- spec/system/homepage_system_spec.rb
+++ spec/system/homepage_system_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe "Home Page", type: :system do
 
-  before(:all) do
+  before(:each) do
       product1 = create(:product, name: 'product_1')
       product2 = create(:product, name: 'product_2')
   end
@@ -10,7 +10,9 @@ RSpec.describe "Home Page", type: :system do
   it "displays all available Products" do
     visit "/"
 
-    expect(page).to have_text("product_1")
-    expect(page).to have_text("product_2")
+    assert_selector "h4.card-title", text: "product_1"
+    assert_selector "h4.card-title", text: "product_2"
+    assert_selector "h4.card-title", count: 2
   end
 end
+
