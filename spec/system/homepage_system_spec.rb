b30ae85fb509f27d9da444566cf7b4028b58ec8e
ciejj <wozniczka.mateusz@gmail.com>
2021-05-25 13:57:15 +0000

    refactor: clean code with rubocop

--- spec/system/homepage_system_spec.rb
+++ spec/system/homepage_system_spec.rb
@@ -1,18 +1,16 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe "Home Page", type: :system do
-
+RSpec.describe 'Home Page', type: :system do
   before(:each) do
-      product1 = create(:product, name: 'product_1')
-      product2 = create(:product, name: 'product_2')
+    create(:product, name: 'product_1')
+    create(:product, name: 'product_2')
   end
 
-  it "displays all available Products" do
-    visit "/"
+  it 'displays all available Products' do
+    visit '/'
 
-    assert_selector "h4.card-title", text: "product_1"
-    assert_selector "h4.card-title", text: "product_2"
-    assert_selector "h4.card-title", count: 2
+    assert_selector 'h4.card-title', text: 'product_1'
+    assert_selector 'h4.card-title', text: 'product_2'
+    assert_selector 'h4.card-title', count: 2
   end
 end
-
