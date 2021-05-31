db7033ef41eb2412e488c4d81224948f69ae9e64
ciejj <wozniczka.mateusz@gmail.com>
2021-05-31 21:41:14 +0000

    feat: add 'Log In' button to navbar

---
+++ spec/system/login_system_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe 'Login Page', type: :system do
+  
+  context 'when no visitng the Home Page' do 
+    it 'Log In button is visible' do
+      visit '/'
+  
+      expect(page).to have_link('Log In')
+    end
+  end
+ 
+end
