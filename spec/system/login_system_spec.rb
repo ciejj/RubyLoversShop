fda4b66846a8625fea8b404f3fd6eb78a478a1fe
ciejj <wozniczka.mateusz@gmail.com>
2021-06-02 23:58:43 +0000

    test: user can reset password

--- spec/system/login_system_spec.rb
+++ spec/system/login_system_spec.rb
@@ -12,6 +12,15 @@ RSpec.describe 'Login Page', type: :system do
       find('input[name="commit"]').click
       expect(page).to have_content('Signed in successfully')
     end
+
+    it 'user can reset password' do
+      visit '/'
+      click_on('Log In')
+      click_on('Forgot Password?')
+      fill_in 'user_email', with: user.email
+      find('input[name="commit"]').click
+      expect(page).to have_content('You will receive an email with instructions on how to reset your password in a few minutes.')
+    end
   end
 
   context 'when providing invalid details' do
@@ -27,14 +36,21 @@ RSpec.describe 'Login Page', type: :system do
     it 'user cannot Log In with invalid email' do
       visit '/'
       click_on('Log In')
-      fill_in 'user_email', with: 'fake_exmail@example.com'
+      fill_in 'user_email', with: 'fake_email@example.com'
       fill_in 'user_password', with: 'wrong_password'
       find('input[name="commit"]').click
       expect(page).to have_content('Invalid Email or password')
     end
+
+    it 'user cannot reset password' do
+      visit '/'
+      click_on('Log In')
+      click_on('Forgot Password?')
+      fill_in 'user_email', with: 'fake_email@example.com'
+      find('input[name="commit"]').click
+      expect(page).to have_content('Email not found')
+    end
   end
-
-
 
   context 'when visitng the Home Page' do 
     it 'Log In button is visible' do
@@ -52,5 +68,4 @@ RSpec.describe 'Login Page', type: :system do
       expect(page).to have_link('Log In')
     end
   end
- 
 end
