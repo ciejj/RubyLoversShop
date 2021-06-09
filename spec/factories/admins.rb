49e35075b568ae325620bb1fb89462f6c2d054f8
ciejj <wozniczka.mateusz@gmail.com>
2021-06-09 22:15:48 +0000

    test: add system spec for admin dashboard access

--- spec/factories/admins.rb
+++ spec/factories/admins.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :admin do
-    
+    sequence(:email) { |n| "admin_no_#{n}@example.com" }
+    password { 'password' }
   end
 end
---
+++ spec/system/admin_dashboard_system_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+
+RSpec.describe 'Admin Dashboard', type: :system do
+  let!(:user) { create(:user) }
+  let!(:admin) { create(:admin) }
+
+  context 'when not logged in' do
+    before do
+      visit 'admin'
+    end
+
+    it 'redirects to root path' do
+      expect(page).to have_current_path(root_path)
+    end
+
+    it 'displays unauthorized warning' do
+      expect(page).to have_content('You are not authorized.')
+    end
+  end
+
+  context 'when logged in as user' do
+    before do
+      visit '/'
+      click_on('Log In')
+      fill_in 'user_email', with: user.email
+      fill_in 'user_password', with: 'password'
+      find('input[name="commit"]').click
+      visit 'admin'
+    end
+
+    it 'redirects to root path' do
+      expect(page).to have_current_path(root_path)
+    end
+
+    it 'displays unauthorized warning' do
+      expect(page).to have_content('You are not authorized.')
+    end
+  end
+
+  context 'when logged in as admin' do
+    before do
+      visit '/admins/sign_in'
+      fill_in 'admin_email', with: admin.email
+      fill_in 'admin_password', with: 'password'
+      find('input[name="commit"]').click
+    end
+
+    it 'redirects to admin root path' do
+      expect(page).to have_current_path(admin_root_path)
+    end
+
+    it 'displays sign in confirmation' do
+      expect(page).to have_content('Signed in successfully.')
+    end
+  end
+end
