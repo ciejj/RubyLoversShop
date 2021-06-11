d8433c10f73141b1f4711096c8d595bca3d862ee
ciejj <wozniczka.mateusz@gmail.com>
2021-06-11 12:42:41 +0000

    feat: add product index to admin dashboard

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -2,9 +2,10 @@ module Admin
   class ProductsController < ApplicationController
     layout 'admin'
     before_action :authenticate_administrator!
-  
+
     def index
+      @products = Product.all
     end
-  
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   devise_for :administrators
 
   devise_for :users
+
   root to: "pages#home"
 
   namespace :admin do
     root to: "products#index", :as => :administrator_root
+    resources :products
   end
 end
