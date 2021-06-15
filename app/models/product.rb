1eff7d23fba52d5d9550cb4c09baf1b58958080f
ciejj <wozniczka.mateusz@gmail.com>
2021-06-15 13:43:02 +0000

    feat: make brand and category optional for product

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,8 +2,8 @@ class Product < ApplicationRecord
   validates :name, presence: true
   validates :price, presence: true
   has_one_attached :main_image
-  belongs_to :category
-  belongs_to :brand
+  belongs_to :category, optional: true
+  belongs_to :brand, optional: true
   scope :filter_by_category, -> (category_id) { where category_id: category_id }
   scope :filter_by_brand, -> (brand_id) { where brand_id: brand_id }
 
