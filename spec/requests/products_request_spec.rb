139c714b30d45e4dc154c5f2bbdc887a6f653e4a
ciejj <wozniczka.mateusz@gmail.com>
2021-06-16 23:30:38 +0000

    test: refactor product tests

--- spec/requests/products_request_spec.rb
+++ spec/requests/products_request_spec.rb
@@ -1,35 +1,28 @@
 require 'rails_helper'
 
-RSpec.describe 'POST /admin/products', type: :request do
+RSpec.describe 'Products', type: :request do
   before do
     login_as(create(:administrator))
   end
 
-  context 'when attributes are valid' do
-    it 'creates new product' do 
-      post '/admin/products', params: {
-        product: { name: 'new_product', price: '100' }
-      }
+  describe 'POST /admin/products' do
+    let(:product) { create(:product) }
 
-      expect(Product.count).to eq(1)
-    end
-  end
+    context 'when attributes are valid' do
+      before do
+        post '/admin/products', params: {
+          product: { name: product.name, price: product.price }
+        }
+      end
 
-  context 'when attributes are invalid' do
-    it 'does not create a new product without a name' do
-      post '/admin/products', params: {
-        product: { price: '100' }
-      }
+      it 'redirects to admin/products' do
+        expect(response).to redirect_to(admin_products_path) 
+      end
 
-      expect(Product.count).to eq(0)
-    end
-
-    it 'does not create a new product without a price' do
-      post '/admin/products', params: {
-        product: { name: 'new_product' }
-      }
-
-      expect(Product.count).to eq(0)
+      it 'adds new product' do
+        follow_redirect!
+        expect(response.body).to include(product.name)
+      end
     end
   end
 end
--- spec/system/products_system_spec.rb
+++ spec/system/products_system_spec.rb
@@ -3,33 +3,71 @@
 require 'rails_helper'
 
 RSpec.describe 'Products', type: :system do
-  context 'when creating a new one' do
+  context 'when creating a new product' do
     before do
       login_as(create(:administrator))
       visit admin_products_path
       find('#add-product-button').click
     end
 
-    it 'is successfull with valid attributes' do
-      fill_in 'product_name', with: 'test_product'
-      fill_in 'product_price', with: '100'
-      click_button 'Create Product'
+    context 'with valid attributes' do
+      let(:product_name) { 'new_product' }
+      let(:product_price) { 100 }
 
-      expect(page).to have_content 'Product added successfully'
+      before do
+        fill_in 'product_name', with: product_name
+        fill_in 'product_price', with: product_price
+        click_button 'Create Product'
+      end
+
+      it 'is created' do
+        expect(page).to have_content 'Product added successfully'
+      end
+
+      it 'is visible on the admin index page' do
+        expect(page).to have_content product_name
+      end
+
+      it 'is visible on the home page' do
+        visit '/'
+        expect(page).to have_content product_name
+      end
     end
 
-    it 'fails with missing name' do
-      fill_in 'product_price', with: '100'
-      click_button 'Create Product'
+    context 'when the price is missing' do    
+      let(:product_name) { 'product_with_no_price' }
 
-      expect(page).to have_content 'Name can\'t be blank'
+      before do
+        fill_in 'product_name', with: product_name
+        click_button 'Create Product'
+      end
+
+      it 'displays an error' do
+        expect(page).to have_content 'Price can\'t be blank'
+      end
+
+      it 'is not adding new product' do
+        visit '/'
+        expect(page).to have_no_content product_name
+      end
     end
+    
+    context 'when the price is missing' do    
+      let(:product_price) { 1 }
 
-    it 'fails with missing price' do
-      fill_in 'product_name', with: 'test_product'
-      click_button 'Create Product'
+      before do
+        fill_in 'product_price', with: product_price
+        click_button 'Create Product'
+      end
 
-      expect(page).to have_content 'Price can\'t be blank'
+      it 'displays an error' do
+        expect(page).to have_content 'Name can\'t be blank'
+      end
+
+      it 'is not adding new product' do
+        visit '/'
+        expect(page).to have_no_content product_price
+      end
     end
   end
 end
