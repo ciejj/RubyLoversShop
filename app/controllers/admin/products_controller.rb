c4cd3ee6f886565dcb430936b54df8fcc5fb272a
ciejj <wozniczka.mateusz@gmail.com>
2021-06-17 14:29:13 +0000

    feat: add prodcut creation

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Admin
   class ProductsController < ApplicationController
     layout 'admin'
@@ -7,5 +9,24 @@ module Admin
       @products = Product.includes([:category]).includes([:brand]).all
     end
 
+    def new
+      @product = Product.new
+    end
+
+    def create
+      @product = Product.new(product_params)
+
+      if @product.save
+        redirect_to admin_products_path, notice: 'Product added successfully'
+      else
+        render :new
+      end
+    end
+
+    private
+
+    def product_params
+      params.require(:product).permit(:name, :price, :category_id, :brand_id, :main_image)
+    end
   end
 end
