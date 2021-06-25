1b3ec7b90e2e75c63fc278ac1f73cccc7ede5f29
ciejj <wozniczka.mateusz@gmail.com>
2021-06-25 23:01:39 +0000

    feat: add product images to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,9 @@
+require 'open-uri'
+
 Administrator.create_with(password: 'password').find_or_create_by(email: 'admin@admin.com')
 User.create_with(password: 'password').find_or_create_by(email: 'user@admin.com')
+
+puts 'generating Categories'
 
 Category.destroy_all
 4.times do
@@ -7,6 +11,8 @@ Category.destroy_all
     name: Faker::Commerce.unique.department(max: 1)
   )
 end
+
+puts 'generating Brands'
 
 Brand.destroy_all
 4.times do
@@ -20,10 +26,14 @@ brands = Brand.all
 
 Product.destroy_all
 15.times do 
-  Product.create(
+  p = Product.create(
       name: Faker::Commerce.unique.product_name,
       price: Faker::Number.decimal(l_digits: 2),
       category: categories[rand(4)],
       brand: brands[rand(4)]
   )
+  puts "generating - product - #{p.name}"
+  downloaded_image = URI.open("https://source.unsplash.com/700x400/?#{p.name.split.last}")
+  p.main_image.attach(io: downloaded_image, filename: "mi_#{p.id}.png")
+
 end
