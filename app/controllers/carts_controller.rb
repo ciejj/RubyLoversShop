a79ba60d77f0a7872f0d9763955cc002ae971b53
ciejj <wozniczka.mateusz@gmail.com>
2021-07-08 11:55:57 +0000

    refactor: add product to cart by interactor

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -9,14 +9,10 @@ class CartsController < ApplicationController
 
   def add
     product = Product.find(params[:product_id])
-    item_added = @cart.add_product(product)
+    result = AddProductToCart.call(product: product, cart: @cart)
 
-    if item_added.nil?
-      flash[:notice] = "#{product.name} is already in the cart"
-    else
-      item_added.save
-      flash[:notice] = "Added #{product.name} to the cart"
-    end
+    flash[:notice] = result.message
+
     redirect_back(fallback_location: root_path)
   end
 
