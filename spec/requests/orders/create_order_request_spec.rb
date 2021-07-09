6b85b97f0e6d1766b42a865342bbba57d67d41f1
ciejj <wozniczka.mateusz@gmail.com>
2021-07-09 23:30:46 +0000

    test: add create order specs

---
+++ spec/requests/orders/create_order_request_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'POST /orders', type: :request do
+  context 'when logged in as user' do
+    let!(:product) { create(:product) }
+    let!(:user) { create(:user) }
+
+    before do
+      login_as(user, scope: :user)
+    end
+
+    context 'with a product in the cart' do
+      before do
+        post '/cart', params: { product_id: product.id }
+      end
+
+      it 'creates new Order' do
+        expect do
+          post '/orders'
+        end.to change(Order, :count).by(1)
+      end
+    end
+
+    context 'without a product in the cart' do
+      it 'does not create new Order' do
+        expect do
+          post '/orders'
+        end.not_to change(Order, :count)
+      end
+    end
+  end
+end
