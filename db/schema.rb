3cc389b25d77c62ec0972be4725362fd84a3b576
ciejj <wozniczka.mateusz@gmail.com>
2021-07-09 23:10:37 +0000

    refactor: rework orders state enum

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_09_152910) do
+ActiveRecord::Schema.define(version: 2021_07_09_213849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,8 +81,13 @@ ActiveRecord::Schema.define(version: 2021_07_09_152910) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-# Could not dump table "orders" because of following StandardError
-#   Unknown type 'order_state' for column 'state'
+  create_table "orders", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "state", default: 0
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
