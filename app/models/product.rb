0a103c3dcdf26909759ee071457ddc378062d889
ciejj <wozniczka.mateusz@gmail.com>
2021-07-22 14:42:28 +0000

    feat: add order items to order

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,6 +9,7 @@ class Product < ApplicationRecord
   scope :filter_by_category, ->(category_id) { where category_id: category_id }
   scope :filter_by_brand, ->(brand_id) { where brand_id: brand_id }
   has_many :cart_items, dependent: :destroy
+  has_many :order_items, dependent: :destroy
 
   def main_image_path
     main_image.attached? ? main_image : 'main_image_placeholder.png'
