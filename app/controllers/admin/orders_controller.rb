97c7934a64a050a1e79a467122870a1e617705b5
ciejj <wozniczka.mateusz@gmail.com>
2021-07-22 22:52:22 +0000

    feat: add order show page to admin dashboard

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -5,5 +5,10 @@ module Admin
     def index
       @pagy, @orders = pagy(Order.order(created_at: :desc))
     end
+
+    def show
+      @order = Order.find(params[:id])
+      @order_items = @order.order_items.includes(:product)
+    end
   end
 end
