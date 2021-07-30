8a62fa5b6ea2f53d4abd5f104e3497ccc1e39b4b
ciejj <wozniczka.mateusz@gmail.com>
2021-07-30 20:58:01 +0000

    feat: move payment state control to service

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -3,18 +3,4 @@
 class Payment < ApplicationRecord
   belongs_to :order
 
-  include AASM
-
-  aasm do
-    state :pending, initial: true
-    state :failed, :completed
-
-    event :fail do
-      transitions from: :pending, to: :failed
-    end
-
-    event :complete do
-      transitions from: :pending, to: :completed
-    end
-  end
 end
--- db/migrate/20210723153558_add_aasm_state_to_payments.rb
+++
@@ -1,5 +0,0 @@
-class AddAasmStateToPayments < ActiveRecord::Migration[6.1]
-  def change
-    add_column :payments, :aasm_state, :string
-  end
-end
