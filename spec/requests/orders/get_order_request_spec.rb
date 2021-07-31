e702c6d2ce74c5c46d4712966e9ed7e0af30fd05
ciejj <wozniczka.mateusz@gmail.com>
2021-07-31 22:14:41 +0000

    test: refactor order factory

--- spec/requests/orders/get_order_request_spec.rb
+++ spec/requests/orders/get_order_request_spec.rb
@@ -5,7 +5,6 @@ require_relative '../shared/request_restricted_to_administrators'
 
 RSpec.describe 'GET /orders/:id', type: :request do
   let!(:order) { create(:order) }
-  let!(:order_item) { create(:order_item, order: order) }
 
   context 'when logged in as administrator' do
     let!(:administrator) { create(:administrator) }
@@ -17,7 +16,7 @@ RSpec.describe 'GET /orders/:id', type: :request do
     it 'returns order\'s details' do
       get "/admin/orders/#{order.id}"
       expect(response.body).to include(order.id.to_s).and include(order.state)
-        .and include(order.user.email).and include(order_item.product.name)
+        .and include(order.user.email).and include(order.order_items.first.product.name)
         .and include(order.total.to_s)
     end
   end
