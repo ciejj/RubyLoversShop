2dc2730dd3e687fa1fbff0c2c325e1a72096bec8
ciejj <wozniczka.mateusz@gmail.com>
2021-08-03 20:42:52 +0000

    feat: link payment to order

---
+++ db/migrate/20210803203150_create_payments.rb
@@ -0,0 +1,10 @@
+class CreatePayments < ActiveRecord::Migration[6.1]
+  def change
+    create_table :payments do |t|
+      t.belongs_to :order, null: false, foreign_key: true
+      t.string :state, default: 'pending'
+
+      t.timestamps
+    end
+  end
+end
