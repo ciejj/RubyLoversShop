5e8a8a700a742da3693fc9f1a84dfdecf1231126
ciejj <wozniczka.mateusz@gmail.com>
2021-08-19 00:45:23 +0000

    test: fix shared example

--- spec/system/shared/order_which_cant_be_completed.rb
+++ spec/system/shared/order_which_cant_be_completed.rb
@@ -4,6 +4,11 @@ require 'rails_helper'
 
 RSpec.shared_examples 'Order which can\'t be completed', type: :system do |payment_state, shipment_state|
   context "when payments is '#{payment_state}' and shipment is \'#{shipment_state}\' " do
+    before do
+      order.payment.update(state: payment_state)
+      order.shipment.update(state: shipment_state)
+    end
+
     it 'changes it\'s state to \'failed\' when Fail button is pressed' do
       within '.order-events' do
         click_link 'Fail'
