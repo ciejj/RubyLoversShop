0d1a565930322ab58937a997c68160aae65fcebf
ciejj <wozniczka.mateusz@gmail.com>
2021-09-09 22:40:18 +0000

    feat: add product quantity cotrol to cart

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -19,9 +19,21 @@ class CartItemsController < ApplicationController
     redirect_back(fallback_location: root_path)
   end
 
+  def update
+    cart_item = CartItem.find(params[:id])
+    cart_item.update!(update_params)
+    redirect_back(fallback_location: cart_path)
+  end
+
   def destroy_all
     CartItem.where(user_id: current_user.id).destroy_all
     flash[:notice] = 'Cart has been emptied'
     redirect_to root_path
+  end
+
+  private
+
+  def update_params
+    params.require(:cart_item).permit(:quantity)
   end
 end
