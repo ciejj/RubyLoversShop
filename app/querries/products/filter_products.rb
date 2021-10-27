78322bc1297b62176989dda8c17b51bf5d0daa92
ciejj <wozniczka.mateusz@gmail.com>
2021-10-27 23:10:21 +0000

    refactor: remove utility function code smell

--- app/querries/products/filter_products.rb
+++ app/querries/products/filter_products.rb
@@ -2,20 +2,24 @@
 
 module Products
   class FilterProducts
+
+    def initialize(scope = Product.all.with_attached_main_image)
+      @scope = scope
+    end
+
     def call(params)
-      scoped = Product.all.with_attached_main_image
-      scoped = filter_by_brand(scoped, params[:brand_id])
-      filter_by_category(scoped, params[:category_id])
+      @scope = filter_by_brand(params[:brand_id])
+      filter_by_category(params[:category_id])
     end
 
     private
 
-    def filter_by_brand(scoped, brand_id)
-      brand_id ? scoped.where(brand_id: brand_id) : scoped
+    def filter_by_brand(brand_id)
+      brand_id ? @scope.where(brand_id: brand_id) : @scope
     end
 
-    def filter_by_category(scoped, category_id)
-      category_id ? scoped.where(category_id: category_id) : scoped
+    def filter_by_category(category_id)
+      category_id ? @scope.where(category_id: category_id) : @scope
     end
   end
 end
