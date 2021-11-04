0741b1979db50941ad8071312e4cd24d20a710ff
ciejj <wozniczka.mateusz@gmail.com>
2021-11-04 23:57:31 +0000

    feat: add filtering by multiple price ranges

--- app/querries/products/filter_products.rb
+++ app/querries/products/filter_products.rb
@@ -2,14 +2,14 @@
 
 module Products
   class FilterProducts
-
     def initialize(scope = Product.all.with_attached_main_image)
       @scope = scope
     end
 
     def call(params)
       @scope = filter_by_brand(params[:brand_id])
-      filter_by_category(params[:category_id])
+      @scope = filter_by_category(params[:category_id])
+      filter_by_price_ranges(params[:price_range_ids])
     end
 
     private
@@ -20,6 +20,15 @@ module Products
 
     def filter_by_category(category_id)
       category_id ? @scope.where(category_id: category_id) : @scope
+    end
+
+    def filter_by_price_ranges(price_range_ids)
+      if price_range_ids
+        price_ranges = price_range_ids.map { |id| PriceRange.find(id).range }
+        @scope.where(price: price_ranges)
+      else
+        @scope
+      end
     end
   end
 end
