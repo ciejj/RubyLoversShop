a9dfb778542e61ecbe22b718e8abfdafea9238a2
ciejj <wozniczka.mateusz@gmail.com>
2021-11-08 15:02:09 +0000

    test: refactor product names

--- spec/system/price_filter_visibility_system_spec.rb
+++ spec/system/price_filter_visibility_system_spec.rb
@@ -5,16 +5,16 @@ require 'rails_helper'
 RSpec.describe 'Price Filter Visibility', type: :system do
   let!(:category) { create(:category) }
   let!(:brand) { create(:brand) }
-  let!(:price_range_1) { create(:price_range, min: 0, max: 10) }
-  let!(:price_range_2) { create(:price_range, min: 11, max: 20) }
+  let!(:price_range1) { create(:price_range, min: 0, max: 10) }
+  let!(:price_range2) { create(:price_range, min: 11, max: 20) }
 
   context 'when no filters are applied' do
     it 'does not display Price Ranges filter' do
       visit '/'
 
       expect(page).to have_no_css('h4.my-4', text: 'Price Ranges')
-        .and have_no_css('.form-check-label', text: price_range_1.name)
-        .and have_no_css('.form-check-label', text: price_range_2.name)
+        .and have_no_css('.form-check-label', text: price_range1.name)
+        .and have_no_css('.form-check-label', text: price_range2.name)
     end
   end
 
@@ -26,8 +26,8 @@ RSpec.describe 'Price Filter Visibility', type: :system do
 
     it 'displays Price Ranges filter' do
       expect(page).to have_css('h4.my-4', text: 'Price Ranges')
-        .and have_css('.form-check-label', text: price_range_1.name)
-        .and have_css('.form-check-label', text: price_range_2.name)
+        .and have_css('.form-check-label', text: price_range1.name)
+        .and have_css('.form-check-label', text: price_range2.name)
     end
   end
 
@@ -39,8 +39,8 @@ RSpec.describe 'Price Filter Visibility', type: :system do
 
     it 'displays Price Ranges filter' do
       expect(page).to have_css('h4.my-4', text: 'Price Ranges')
-        .and have_css('.form-check-label', text: price_range_1.name)
-        .and have_css('.form-check-label', text: price_range_2.name)
+        .and have_css('.form-check-label', text: price_range1.name)
+        .and have_css('.form-check-label', text: price_range2.name)
     end
   end
 
@@ -53,8 +53,8 @@ RSpec.describe 'Price Filter Visibility', type: :system do
 
     it 'displays Price Ranges filter' do
       expect(page).to have_css('h4.my-4', text: 'Price Ranges')
-        .and have_css('.form-check-label', text: price_range_1.name)
-        .and have_css('.form-check-label', text: price_range_2.name)
+        .and have_css('.form-check-label', text: price_range1.name)
+        .and have_css('.form-check-label', text: price_range2.name)
     end
   end
 end
