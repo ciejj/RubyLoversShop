d647d1bf4e41517790c583ef9637a20b0a2a701b
ciejj <wozniczka.mateusz@gmail.com>
2021-11-26 15:40:04 +0000

    refactor: fix rubocop offences

--- spec/requests/orders/create_billing_address_request_spec.rb
+++ spec/requests/orders/create_billing_address_request_spec.rb
@@ -3,9 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe 'POST /orders/billing_address', type: :request do
-    
   subject(:request_call) do
-    post '/orders/billing_address', params: {address: params}
+    post '/orders/billing_address', params: { address: params }
   end
 
   let!(:product) { create(:product) }
@@ -14,7 +13,6 @@ RSpec.describe 'POST /orders/billing_address', type: :request do
     let!(:user) { create(:user) }
 
     context 'with newly created order' do
-      
       before do
         login_as(user, scope: :user)
         post '/cart_items', params: { product_id: product.id }
@@ -23,17 +21,17 @@ RSpec.describe 'POST /orders/billing_address', type: :request do
 
       context 'when posting valid Address details' do
         let(:params) { attributes_for(:address) }
-  
+
         it 'creates new Billing Address' do
           expect do
             request_call
           end.to change(Address, :count).by(1)
         end
       end
-  
+
       context 'when posting invalid Address details' do
         let(:params) { attributes_for(:address, street_name1: '') }
-  
+
         it 'does not create new Billing Address' do
           expect do
             request_call
@@ -43,10 +41,9 @@ RSpec.describe 'POST /orders/billing_address', type: :request do
     end
 
     context 'without newly created order' do
-    
       context 'when posting valid Address details' do
         let(:params) { attributes_for(:address) }
-  
+
         it 'does not create Billing Address' do
           expect do
             request_call
